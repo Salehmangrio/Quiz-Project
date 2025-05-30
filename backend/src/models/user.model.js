7141cdd8d5afe848f5fc67612e220085ca940888
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-30 15:53:07 +0500

    username --> name update

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
