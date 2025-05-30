818beff0a1920f9740cfeced4e70160328ca3186
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-05-30 15:57:29 +0500

    Authentication Route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 const router = Router();
-import { login, register, logout } from '../controller/auth.controller.js';
-import { authMiddleware } from '../middleware/auth.middleware.js';
-import { validateLogin, validateRegister } from '../middleware/validate.middleware.js';
+import { login, register, logout } from '../controllers/auth.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
+import { validateLogin, validateRegister } from '../middlewares/validate.middleware.js';
 
 router.post('/login', validateLogin, login);
 router.post('/register', validateRegister, register);
