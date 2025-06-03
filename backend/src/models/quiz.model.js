ab8caa7cde9aa85788d70c194bed20a3325d63cf
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-03 19:22:53 +0500

    Update: Quiz Model

--- backend/src/models/quiz.model.js
+++ backend/src/models/quiz.model.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const QuizSchema = new mongoose.Schema({
-        title: String,
-        description: String,
-        timeLimit: Number,
-        questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
-        active: { type: Boolean, default: false }
-    },
-    {
-        timestamps: true,
-    });
+    title: { type: String, required: true, minlength: 3, maxlength: 50 },
+    description: { type: String, required: true, minlength: 10, maxlength: 500 },
+    timeLimit: { type: Number, required: true, min: 2 },
+    questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    active: { type: Boolean, default: false },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, {
+    timestamps: true,
+});
 
 const Quiz = mongoose.model("Quiz", QuizSchema);
 export default Quiz;
