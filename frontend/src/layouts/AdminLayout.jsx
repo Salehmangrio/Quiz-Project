f7e22f8d4ae83a91a5cfd8a53468538c5ba2adfc
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-03 23:39:42 +0500

    Add : AdminNavbar in AdminLayout

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AdminNavbar from '../components/AdminNavbar'
 
 const AdminLayout = () => {
     return (
-        <Outlet />
+        <>
+            <AdminNavbar />
+            <Outlet />
+        </>
     )
 }
 
