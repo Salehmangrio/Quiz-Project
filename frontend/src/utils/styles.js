572e77871e5cf503013279373b545d697d56170a
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-05 00:24:33 +0500

    add: reusable style

---
+++ frontend/src/utils/styles.js
@@ -0,0 +1,12 @@
+export const buttonStyles = `
+  w-full  text-white
+  shadow-md hover:shadow-lg transition-shadow duration-300
+  text-center inline-block
+  bg-gradient-to-r from-emerald-400 to-emerald-600
+  px-4 py-2 rounded-lg
+  select-none
+  cursor-pointer
+  font-semibold
+  bg-emerald-500 opacity-80 hover:opacity-100
+  hover:bg-emerald-600
+`
