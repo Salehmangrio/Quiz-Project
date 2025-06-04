dbeb99b49ac4ce5d42ba23468b1b6c986ebe46a4
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 23:39:15 +0500

    fix: name is not unique

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
-        username: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        name: { type: String, required: true, trim: true },
+        username: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, trim: true },
+        password: { type: String, required: true, trim: true },
         role: { type: String, enum: ['user', 'admin'], default: 'user' }
     },
     {
