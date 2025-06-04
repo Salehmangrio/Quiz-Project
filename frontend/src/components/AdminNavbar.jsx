e1ac8e56cb782fe2a5716f21d9a3ad0f84387cfc
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 10:58:22 +0500

    update : minor update

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -9,7 +9,7 @@ const AdminNavbar = () => {
                 <Link to={'.'} className='text-xl font-extrabold'>{user.name}</Link></h1>
             <ul className='flex gap-4'>
                 <li>
-                    <NavLink to={'.'}>ViewQuiz</NavLink>
+                    <NavLink to={'.'}>Vi-m ewQuiz</NavLink>
                 </li>
                 <li>
                     <NavLink to={'create'}>CreateQuiz</NavLink>
