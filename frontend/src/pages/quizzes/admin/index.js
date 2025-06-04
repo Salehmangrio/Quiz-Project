57f08220b9726b29229a083a2ced40bdc0f6d385
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 11:04:46 +0500

    Add: ViewQustions and AddQuestion in index file

--- frontend/src/pages/quizzes/admin/index.js
+++ frontend/src/pages/quizzes/admin/index.js
@@ -1,7 +1,9 @@
 import CreateQuiz from "./CreateQuiz";
 import ViewQuiz from "./ViewQuiz";
 import UpdateQuiz from "./UpdateQuiz";
+import AddQuestion from "./AddQuestion";
+import ViewQuestions from "./ViewQuestions";
 
 export  {
-    CreateQuiz,UpdateQuiz,ViewQuiz
+    CreateQuiz,UpdateQuiz,ViewQuiz,AddQuestion,ViewQuestions
 }
