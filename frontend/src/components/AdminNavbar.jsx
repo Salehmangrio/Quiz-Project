bc3de0ccb0ef61c036d6380516fb894c005c7ffd
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 13:42:50 +0500

    update: active style on NavLink in AdminNavbar

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 
 const AdminNavbar = () => {
+    const activeStyle = ({ isActive }) => isActive ? "font-bold text-rose-500" : ""
+
     const user = JSON.parse(localStorage.getItem('user'))
     return (
         <div className='h-[70px] flex justify-between items-center bg-amber-200 px-6'>
             <h1 className=' font-serif text-black capitalize'>
-                <Link to={'.'} className='text-xl font-extrabold'>{user.name}</Link></h1>
+                <Link to={'.'} className='text-xl font-extrabold' >{user.name}</Link></h1>
             <ul className='flex gap-4'>
                 <li>
-                    <NavLink to={'.'}>ViewQuiz</NavLink>
+                    <NavLink to={'.'} end className={activeStyle} >ViewQuiz</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'create'}>CreateQuiz</NavLink>
+                    <NavLink to={'create'} className={activeStyle}>CreateQuiz</NavLink>
                 </li>
             </ul>
         </div>
