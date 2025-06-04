0a36eb05841a9fe0fefcf5fc28673e3ecc5f1757
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 23:40:35 +0500

    add: navbar for user

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 
 const UserLayout = () => {
+    const navigate = useNavigate()
+    const user = JSON.parse(localStorage.getItem('user'))
     return (
-        <Outlet />
+        <>
+            <nav className='w-full bg-gray-800 text-white p-4 flex justify-between items-center'>
+                <h1 className='text-2xl font-bold'>Welcome, {user ? user.name : 'Guest'}</h1>
+                <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+                    onClick={() => {
+                        localStorage.removeItem('user')
+                        navigate('/login',{ replace: true})
+                    }}>Logout</button>
+            </nav>
+            <Outlet />
+        </>
     )
 }
 
