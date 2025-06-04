81538509c9e385658ba6dd39860de351160ce5fb
Saleh Muhammad Mangrio <salehmangrio114@gmail.com>
2025-06-04 10:56:13 +0500

    Update: more properties on fieldsof QuestionModel

--- backend/src/models/question.model.js
+++ backend/src/models/question.model.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 
 const QuestionSchema = new mongoose.Schema({
   quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
-  text: String,
-  subject: String,
-  options: [String],
-  correctAnswerIndex: Number
+  text: { type: String, required: true },
+  subject: { type: String, required: true },
+  options: { type: [String], required: true },
+  correctAnswerIndex: { type: Number, required: true }
 },
-{
+  {
     timestamps: true,
-});
+  });
 
 const Question = mongoose.model('Question', QuestionSchema);
 export default Question;
